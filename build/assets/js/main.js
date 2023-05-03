6d52aa0b0b1c1cb20b480670df4b70622c3d82f9
roy <roy.framery@gmail.com>
2023-05-03 19:16:52 +0200

    end of chapter 3 : learn more types cover array, tuple, object and enums

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,26 +1,86 @@
 "use strict";
-let myName = 'Dave';
-let meaningOfLife;
-let isLoading;
-let album;
-let unionType;
-// Value of myName can only be a string
-myName = 'Roy';
-// Value of meaningOfLife can only be a number
-meaningOfLife = 42;
-// Value of isLoading can only be a booler
-isLoading = true;
-// Value of album can be a string, number or boolean
-album = 'Van Halen';
-album = 1989;
-album = true;
-// Value of unionType can be a string or a number
-unionType = 'Im a unionType';
-unionType = 42;
-// it will implicitly say that it will return a number
-const sum1 = (a, b) => a + b;
-// it will implicitly say that it will return a string
-const sum2 = (a, b) => a + b;
-// when you don't know which type you can assign a variable
-// Intellisense can make a suggestion
-let regularExpression = /\w+/g;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1989, true];
+// We'll get error
+stringArr[0] = 42;
+stringArr.push(42);
+stringArr = guitars;
+guitars[0] = false;
+guitars.unshift(['fender']);
+guitars = mixedData;
+mixedData[0] = { name: 'Dave' };
+mixedData.unshift([false]);
+// empty array is defined as a any type array
+let test = [];
+test = [...guitars, ...stringArr, ...mixedData];
+// We can defined in advance the type of an empty array
+let bands = [];
+bands.push(42); // we have an error
+bands.push('ACDC'); // we don't have an error
+// tuple
+// Is used to defined a specfic position and a specific length
+// here the array must have to contain 3 elements
+// the first one have to by a string
+// the second one have to by a number
+// the last one have to by a boolean
+let myTuple = ['ecole', 42, true];
+// intellisens show a union type
+let mixed = ['John', 1, false];
+// we don't have an error
+mixed = myTuple;
+// We have an error because mixed can possibly have more or less than 3 elements
+myTuple = mixed;
+// We have an error here because myTuple can have only 3 elements
+myTuple[3] = 42;
+// objects
+let myObj;
+myObj = [];
+myObj = myTuple;
+myObj = {};
+const exampleObj = {
+    prop1: 'Ecole',
+    prop2: 42,
+    prop3: true,
+};
+// errors
+exampleObj.prop1 = 42;
+exampleObj.prop2 = false;
+exampleObj.prop3 = 'ecole';
+let evh = {
+    name: 42,
+    active: 'ecole',
+    albums: [1989, 5150, 'OU812', false],
+};
+let jp = {
+    name: 'Jimmy',
+    albums: ['I', 'II', 'IV'],
+};
+// There are no error because property active is optional thanks to the question mark "?"
+evh = jp;
+const greetGuitarist = (guitarist) => `Hello ${guitarist.name}`;
+console.log(greetGuitarist(jp));
+// We have to check is the property name exist
+// If we don't, we could have an error
+// because the property if optional
+const greetGuitaristName = (guitarist) => {
+    if (guitarist.name)
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    return 'Hello!';
+};
+let hp = {
+    albums: ['I', 'II', 'IV'],
+};
+console.log(greetGuitaristName(hp));
+// Enums
+// "Unlike most TypeScript features, Enums are not a Type-level addition to JavaScript
+// but something added to the language and runtime"
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.B);
--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,28 +1,114 @@
-let myName: string = 'Dave';
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
-let unionType: string | number;
+let stringArr: string[] = ['one', 'hey', 'Dave'];
+let guitars: (string | number)[] = ['Strat', 'Les Paul', 5150];
+let mixedData: (string | number | boolean)[] = ['EVH', 1989, true];
 
-// Value of myName can only be a string
-myName = 'Roy';
-// Value of meaningOfLife can only be a number
-meaningOfLife = 42;
-// Value of isLoading can only be a booler
-isLoading = true;
-// Value of album can be a string, number or boolean
-album = 'Van Halen';
-album = 1989;
-album = true;
-// Value of unionType can be a string or a number
-unionType = 'Im a unionType';
-unionType = 42;
+// We'll get error
+stringArr[0] = 42;
+stringArr.push(42);
+stringArr = guitars;
 
-// it will implicitly say that it will return a number
-const sum1 = (a: number, b: number) => a + b;
-// it will implicitly say that it will return a string
-const sum2 = (a: number, b: string) => a + b;
+guitars[0] = false;
+guitars.unshift(['fender']);
+guitars = mixedData;
 
-// when you don't know which type you can assign a variable
-// Intellisense can make a suggestion
-let regularExpression: RegExp = /\w+/g;
+mixedData[0] = { name: 'Dave' };
+mixedData.unshift([false]);
+
+// empty array is defined as a any type array
+let test = [];
+test = [...guitars, ...stringArr, ...mixedData];
+
+// We can defined in advance the type of an empty array
+let bands: string[] = [];
+bands.push(42); // we have an error
+bands.push('ACDC'); // we don't have an error
+
+// tuple
+// Is used to defined a specfic position and a specific length
+// here the array must have to contain 3 elements
+// the first one have to by a string
+// the second one have to by a number
+// the last one have to by a boolean
+let myTuple: [string, number, boolean] = ['ecole', 42, true];
+
+// intellisens show a union type
+let mixed = ['John', 1, false];
+// we don't have an error
+mixed = myTuple;
+// We have an error because mixed can possibly have more or less than 3 elements
+myTuple = mixed;
+// We have an error here because myTuple can have only 3 elements
+myTuple[3] = 42;
+
+// objects
+let myObj: object;
+myObj = [];
+myObj = myTuple;
+myObj = {};
+
+type MyTypedObj = {
+    prop1: string;
+    prop2: number;
+    prop3: boolean;
+};
+
+const exampleObj: MyTypedObj = {
+    prop1: 'Ecole',
+    prop2: 42,
+    prop3: true,
+};
+// errors
+exampleObj.prop1 = 42;
+exampleObj.prop2 = false;
+exampleObj.prop3 = 'ecole';
+
+// an interface work as a type
+// but and interface :
+//  - will not work for primary types
+//  - is always extendable
+//      while a type cannot be re-opened to add new properties
+interface Guitarist {
+    name?: string;
+    active?: boolean;
+    albums: (string | number)[];
+}
+
+let evh: Guitarist = {
+    name: 42,
+    active: 'ecole',
+    albums: [1989, 5150, 'OU812', false],
+};
+let jp: Guitarist = {
+    name: 'Jimmy',
+    albums: ['I', 'II', 'IV'],
+};
+// There are no error because property active is optional thanks to the question mark "?"
+evh = jp;
+
+const greetGuitarist = (guitarist: Guitarist) => `Hello ${guitarist.name}`;
+console.log(greetGuitarist(jp));
+// We have to check is the property name exist
+// If we don't, we could have an error
+// because the property if optional
+const greetGuitaristName = (guitarist: Guitarist) => {
+    if (guitarist.name) return `Hello ${guitarist.name.toUpperCase()}`;
+    return 'Hello!';
+};
+let hp: Guitarist = {
+    albums: ['I', 'II', 'IV'],
+};
+console.log(greetGuitaristName(hp));
+
+// Enums
+// "Unlike most TypeScript features, Enums are not a Type-level addition to JavaScript
+// but something added to the language and runtime"
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.B);
