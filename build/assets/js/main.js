305e9620af6757003189ffc12a440893a1601f5b
roy <roy.framery@gmail.com>
2023-05-03 17:52:11 +0200

    End of the chapter 2 : learn the basic types cover string, number and boolean type. But also union type, RegExp and what a function might also return in addition to what it receives as parametters.

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,16 +1,26 @@
 "use strict";
-let username = 'Dave';
-console.log(username);
-let a = 12;
-// let b: string = '6';
-let b = 6;
-let c = 2;
-/**
- * I get error from TypeScript
- * because of the type of the variable b (type string)
- * In order to fix the issue,
- * I can change the type string to number
- * and change the value
- */
-console.log(a / b);
-console.log(c * b);
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+let unionType;
+// Value of myName can only be a string
+myName = 'Roy';
+// Value of meaningOfLife can only be a number
+meaningOfLife = 42;
+// Value of isLoading can only be a booler
+isLoading = true;
+// Value of album can be a string, number or boolean
+album = 'Van Halen';
+album = 1989;
+album = true;
+// Value of unionType can be a string or a number
+unionType = 'Im a unionType';
+unionType = 42;
+// it will implicitly say that it will return a number
+const sum1 = (a, b) => a + b;
+// it will implicitly say that it will return a string
+const sum2 = (a, b) => a + b;
+// when you don't know which type you can assign a variable
+// Intellisense can make a suggestion
+let regularExpression = /\w+/g;
--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,17 +1,28 @@
-let username = 'Dave';
-console.log(username);
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+let unionType: string | number;
 
-let a: number = 12;
-// let b: string = '6';
-let b: number = 6;
-let c: number = 2;
-/**
- * I get error from TypeScript
- * because of the type of the variable b (type string)
- * In order to fix the issue,
- * I can change the type string to number
- * and change the value
- */
+// Value of myName can only be a string
+myName = 'Roy';
+// Value of meaningOfLife can only be a number
+meaningOfLife = 42;
+// Value of isLoading can only be a booler
+isLoading = true;
+// Value of album can be a string, number or boolean
+album = 'Van Halen';
+album = 1989;
+album = true;
+// Value of unionType can be a string or a number
+unionType = 'Im a unionType';
+unionType = 42;
 
-console.log(a / b);
-console.log(c * b);
+// it will implicitly say that it will return a number
+const sum1 = (a: number, b: number) => a + b;
+// it will implicitly say that it will return a string
+const sum2 = (a: number, b: string) => a + b;
+
+// when you don't know which type you can assign a variable
+// Intellisense can make a suggestion
+let regularExpression: RegExp = /\w+/g;
