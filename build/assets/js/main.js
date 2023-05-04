9016dfdd689e04c6122adda13a505578af8ced9c
roy <roy.framery@gmail.com>
2023-05-04 15:20:05 +0200

    end of the chapter 4: learn more types literal types, type aliases, functions and never type

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,86 +1,88 @@
 "use strict";
-let stringArr = ['one', 'hey', 'Dave'];
-let guitars = ['Strat', 'Les Paul', 5150];
-let mixedData = ['EVH', 1989, true];
-// We'll get error
-stringArr[0] = 42;
-stringArr.push(42);
-stringArr = guitars;
-guitars[0] = false;
-guitars.unshift(['fender']);
-guitars = mixedData;
-mixedData[0] = { name: 'Dave' };
-mixedData.unshift([false]);
-// empty array is defined as a any type array
-let test = [];
-test = [...guitars, ...stringArr, ...mixedData];
-// We can defined in advance the type of an empty array
-let bands = [];
-bands.push(42); // we have an error
-bands.push('ACDC'); // we don't have an error
-// tuple
-// Is used to defined a specfic position and a specific length
-// here the array must have to contain 3 elements
-// the first one have to by a string
-// the second one have to by a number
-// the last one have to by a boolean
-let myTuple = ['ecole', 42, true];
-// intellisens show a union type
-let mixed = ['John', 1, false];
-// we don't have an error
-mixed = myTuple;
-// We have an error because mixed can possibly have more or less than 3 elements
-myTuple = mixed;
-// We have an error here because myTuple can have only 3 elements
-myTuple[3] = 42;
-// objects
-let myObj;
-myObj = [];
-myObj = myTuple;
-myObj = {};
-const exampleObj = {
-    prop1: 'Ecole',
-    prop2: 42,
-    prop3: true,
+// But we can't use a type in an interface
+// interfaces think about those more as object or classes
+// interface PostId = StringOrNumber
+// literal types
+// it's a literal assignment
+let myName; // it's not reassignable
+let userName; // now more than one value can be assigned
+userName = 'Rachel'; // but not this one :P
+userName = 'Amy';
+// function
+// we can type the return
+const add = (a, b) => {
+    return a + b;
 };
-// errors
-exampleObj.prop1 = 42;
-exampleObj.prop2 = false;
-exampleObj.prop3 = 'ecole';
-let evh = {
-    name: 42,
-    active: 'ecole',
-    albums: [1989, 5150, 'OU812', false],
+// here the return is void because we don't have a return
+// So this is a side effect
+const logMsg = (message) => {
+    console.log(message);
 };
-let jp = {
-    name: 'Jimmy',
-    albums: ['I', 'II', 'IV'],
+logMsg('hello');
+logMsg(add(26, 16));
+const subtract = function (c, d) {
+    return c - d;
 };
-// There are no error because property active is optional thanks to the question mark "?"
-evh = jp;
-const greetGuitarist = (guitarist) => `Hello ${guitarist.name}`;
-console.log(greetGuitarist(jp));
-// We have to check is the property name exist
-// If we don't, we could have an error
-// because the property if optional
-const greetGuitaristName = (guitarist) => {
-    if (guitarist.name)
-        return `Hello ${guitarist.name.toUpperCase()}`;
-    return 'Hello!';
+// it's not pertinent here to use an interface because it's using basic types
+// interface mathFunction {
+//     (a: number, b: number): number;
+// }
+let multiply = function (e, f) {
+    return e * f;
 };
-let hp = {
-    albums: ['I', 'II', 'IV'],
+// optional parameters
+// if we have an optional parameter, it needs to be the last in the list
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined')
+        return a + b + c;
+    return a + b;
 };
-console.log(greetGuitaristName(hp));
-// Enums
-// "Unlike most TypeScript features, Enums are not a Type-level addition to JavaScript
-// but something added to the language and runtime"
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.B);
+// default param value
+const sumAll = (a = 7, b, c = 23) => {
+    return a + b + c;
+};
+logMsg(addAll(12, 7, 23));
+logMsg(addAll(12, 7));
+logMsg(sumAll(12, 7));
+// like a have a default value, we have to skip the value by using undefined
+logMsg(sumAll(undefined, 12));
+// Rest Parameters
+// as the optional parameter, rest operator chould come at the end
+// and have the required parameters at the beginning
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+// we are not putting an array here but it's represented in the function as an array
+// because of the rest operator
+logMsg(total(1, 2, 3, 4, 5, 6, 7, 8, 6));
+// never type is essentially for function that explicitly thow errors
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+// But, a function that has an infinite or endless loop inside
+// will have a never type as a return
+// const infinite = () => {
+//     let i: number = 1
+//     while (true) {
+//         i++
+//     }
+// }
+// using a custom type guard to handle condition
+const isNumber = (value) => {
+    return typeof value === 'number' ? true : false;
+};
+const isString = (value) => {
+    return typeof value === 'string' ? true : false;
+};
+// yse if tge bever type
+const numberOrString = (value) => {
+    if (isString(value))
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    // here we return a never type because we don't have an explicit return
+    //  and we don't handling a potential undefined
+    return createError('this should never happen!');
+};
+logMsg(numberOrString('458'));
+logMsg(numberOrString(458));
--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,114 +1,113 @@
-let stringArr: string[] = ['one', 'hey', 'Dave'];
-let guitars: (string | number)[] = ['Strat', 'Les Paul', 5150];
-let mixedData: (string | number | boolean)[] = ['EVH', 1989, true];
+// Type Aliases
+type StringOrNumber = string | number;
 
-// We'll get error
-stringArr[0] = 42;
-stringArr.push(42);
-stringArr = guitars;
+type StringOrNumberArray = (string | number)[];
 
-guitars[0] = false;
-guitars.unshift(['fender']);
-guitars = mixedData;
-
-mixedData[0] = { name: 'Dave' };
-mixedData.unshift([false]);
-
-// empty array is defined as a any type array
-let test = [];
-test = [...guitars, ...stringArr, ...mixedData];
-
-// We can defined in advance the type of an empty array
-let bands: string[] = [];
-bands.push(42); // we have an error
-bands.push('ACDC'); // we don't have an error
-
-// tuple
-// Is used to defined a specfic position and a specific length
-// here the array must have to contain 3 elements
-// the first one have to by a string
-// the second one have to by a number
-// the last one have to by a boolean
-let myTuple: [string, number, boolean] = ['ecole', 42, true];
-
-// intellisens show a union type
-let mixed = ['John', 1, false];
-// we don't have an error
-mixed = myTuple;
-// We have an error because mixed can possibly have more or less than 3 elements
-myTuple = mixed;
-// We have an error here because myTuple can have only 3 elements
-myTuple[3] = 42;
-
-// objects
-let myObj: object;
-myObj = [];
-myObj = myTuple;
-myObj = {};
-
-type MyTypedObj = {
-    prop1: string;
-    prop2: number;
-    prop3: boolean;
-};
-
-const exampleObj: MyTypedObj = {
-    prop1: 'Ecole',
-    prop2: 42,
-    prop3: true,
-};
-// errors
-exampleObj.prop1 = 42;
-exampleObj.prop2 = false;
-exampleObj.prop3 = 'ecole';
-
-// an interface work as a type
-// but and interface :
-//  - will not work for primary types
-//  - is always extendable
-//      while a type cannot be re-opened to add new properties
-interface Guitarist {
+// We can use a type in an other type
+type Guitarist = {
     name?: string;
     active?: boolean;
-    albums: (string | number)[];
-}
-
-let evh: Guitarist = {
-    name: 42,
-    active: 'ecole',
-    albums: [1989, 5150, 'OU812', false],
+    albums: StringOrNumberArray;
 };
-let jp: Guitarist = {
-    name: 'Jimmy',
-    albums: ['I', 'II', 'IV'],
+type UserId = StringOrNumber;
+
+// But we can't use a type in an interface
+// interfaces think about those more as object or classes
+// interface PostId = StringOrNumber
+
+// literal types
+// it's a literal assignment
+let myName: 'Dave'; // it's not reassignable
+
+let userName: 'Dave' | 'John' | 'Amy'; // now more than one value can be assigned
+userName = 'Rachel'; // but not this one :P
+userName = 'Amy';
+
+// function
+// we can type the return
+const add = (a: number, b: number): number => {
+    return a + b;
 };
-// There are no error because property active is optional thanks to the question mark "?"
-evh = jp;
-
-const greetGuitarist = (guitarist: Guitarist) => `Hello ${guitarist.name}`;
-console.log(greetGuitarist(jp));
-// We have to check is the property name exist
-// If we don't, we could have an error
-// because the property if optional
-const greetGuitaristName = (guitarist: Guitarist) => {
-    if (guitarist.name) return `Hello ${guitarist.name.toUpperCase()}`;
-    return 'Hello!';
+// here the return is void because we don't have a return
+// So this is a side effect
+const logMsg = (message: any): void => {
+    console.log(message);
 };
-let hp: Guitarist = {
-    albums: ['I', 'II', 'IV'],
+
+logMsg('hello');
+logMsg(add(26, 16));
+
+const subtract = function (c: number, d: number): number {
+    return c - d;
 };
-console.log(greetGuitaristName(hp));
 
-// Enums
-// "Unlike most TypeScript features, Enums are not a Type-level addition to JavaScript
-// but something added to the language and runtime"
+type mathFunction = (a: number, b: number) => number;
+// it's not pertinent here to use an interface because it's using basic types
+// interface mathFunction {
+//     (a: number, b: number): number;
+// }
+let multiply: mathFunction = function (e, f) {
+    return e * f;
+};
 
-enum Grade {
-    U = 1,
-    D,
-    C,
-    B,
-    A,
-}
+// optional parameters
+// if we have an optional parameter, it needs to be the last in the list
+const addAll = (a: number, b: number, c?: number): number => {
+    if (typeof c !== 'undefined') return a + b + c;
+    return a + b;
+};
 
-console.log(Grade.B);
+// default param value
+const sumAll = (a: number = 7, b: number, c: number = 23): number => {
+    return a + b + c;
+};
+
+logMsg(addAll(12, 7, 23));
+logMsg(addAll(12, 7));
+logMsg(sumAll(12, 7));
+// like a have a default value, we have to skip the value by using undefined
+logMsg(sumAll(undefined, 12));
+
+// Rest Parameters
+// as the optional parameter, rest operator chould come at the end
+// and have the required parameters at the beginning
+const total = (a: number, ...nums: number[]): number => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+// we are not putting an array here but it's represented in the function as an array
+// because of the rest operator
+logMsg(total(1, 2, 3, 4, 5, 6, 7, 8, 6));
+
+// never type is essentially for function that explicitly thow errors
+const createError = (errMsg: string): never => {
+    throw new Error(errMsg);
+};
+
+// But, a function that has an infinite or endless loop inside
+// will have a never type as a return
+// const infinite = () => {
+//     let i: number = 1
+//     while (true) {
+//         i++
+//     }
+// }
+
+// using a custom type guard to handle condition
+const isNumber = (value: any): boolean => {
+    return typeof value === 'number' ? true : false;
+};
+const isString = (value: any): boolean => {
+    return typeof value === 'string' ? true : false;
+};
+
+// yse if tge bever type
+const numberOrString = (value: number | string): string => {
+    if (isString(value)) return 'string';
+    if (isNumber(value)) return 'number';
+    // here we return a never type because we don't have an explicit return
+    //  and we don't handling a potential undefined
+    return createError('this should never happen!');
+};
+
+logMsg(numberOrString('458'));
+logMsg(numberOrString(458));
