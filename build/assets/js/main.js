8f0fa75f1c0871774e333775eb1f6fec17026eff
roy <roy.framery@gmail.com>
2023-05-16 13:51:41 +0200

    End of the chapter 6: Classes Add visibility modifiers, apply interface, use static properties, getters and setters

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,31 +1,113 @@
 "use strict";
-// convert to more or less specific
-let a = 'hello';
-let b = a; // les specific
-let c = a; // more specific
-// angle brackets can be use to define an assertion
-// But it can't be use in TSX files
-let d = 'World';
-let e = 'World';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b; // using '' to coerce the numbers into a string
-};
-// using an assertion because we know that the return will be a string
-let myVal = addOrConcat(2, 2, 'concat');
-// Be careful because TS sees no problem here but a string is return
-let nextVal = addOrConcat(2, 2, 'concat');
-10;
-// double casting or Force casting
-10;
-// the DOM
-const img = document.querySelector('img'); // TS is more specific here and say that it's a HTMLImageElement
-const myImgId = document.querySelector('#myImg'); // TS is less specific and say that it's an Element
-const nextImgId = document.querySelector('#myImg');
-// we will have an error if we remove the exclamation mark,
-// because by default, the element can be a HTML element or null
-// The exclamation mark is for saying that the element can't be null
-img.src;
-// we will have an error if we remove the assertion
-myImgId.src;
+// classes
+// As we can see is this class, there are a redundacy
+// class Coder {
+//     name: string;
+//     music: string;
+//     age: number;
+//     lang: string;
+//     constructor(name: string, music: string, age: number, lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang = lang;
+//     }
+// }
+// To avoid this, we can add what are called visibility modifiers
+class Coder {
+    constructor(name, // can't be modify
+    music, age, // only access in the class
+    lang = 'TypeScript' // access to the class and any object that inherits from it
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+// console.log(Dave.age); // error because age is private
+// console.log(Dave.lang); // error because age is protected
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age); // super needs to come before we try to assign anything else
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+class Guitariste {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitariste('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+////////////////////////////////////////////////////////////////////
+class Peeps {
+    // getCount will be call directly in the class as well
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        // first id will be 1.
+        // Peeps.count++ => the first id will be 0
+        this.id = ++Peeps.count;
+    }
+}
+// count is not apply to any instantiation of the class
+// It applies to the class directly itself
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+// The count is incrementing as much as Peeps class is instantiated
+console.log(Peeps.count);
+console.log(`The Id of Steve is ${Steve.id}`);
+console.log(`The Id of Amy is ${Amy.id}`);
+console.log(`The Id of John is ${John.id}`);
+////////////////////////////////////////
+// getters and setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) &&
+            value.every((el) => typeof el === 'string')) {
+            // return this.dataState = value // setters can't return a value
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+// MyBands.data = 'Van Halen'; // We've got an error because it's not an array
+// MyBands.data = ['Van Halen', 5150]; // We've got the error in the console because there are a number in the array
--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,46 +1,156 @@
-// Type assertion is made when you know more about the type that TypeScript knows
-type One = string;
-type Two = string | number;
-type Three = 'hello';
+// classes
+// As we can see is this class, there are a redundacy
+// class Coder {
+//     name: string;
+//     music: string;
+//     age: number;
+//     lang: string;
 
-// convert to more or less specific
-let a: One = 'hello';
-let b = a as Two; // les specific
-let c = a as Three; // more specific
+//     constructor(name: string, music: string, age: number, lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang = lang;
+//     }
+// }
 
-// angle brackets can be use to define an assertion
-// But it can't be use in TSX files
-let d = <One>'World';
-let e = <string | number>'World';
+// To avoid this, we can add what are called visibility modifiers
+class Coder {
+    // To avoid this redundacy, and you want add a property that you didn't want to instantiate (like a third-party library)
+    // you can use an assertion (exclamation mark here)
+    secondLand!: string;
 
-const addOrConcat = (
-    a: number,
-    b: number,
-    c: 'add' | 'concat'
-): number | string => {
-    if (c === 'add') return a + b;
-    return '' + a + b; // using '' to coerce the numbers into a string
-};
+    constructor(
+        public readonly name: string, // can't be modify
+        public music: string,
+        private age: number, // only access in the class
+        protected lang: string = 'TypeScript' // access to the class and any object that inherits from it
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
 
-// using an assertion because we know that the return will be a string
-let myVal: string = addOrConcat(2, 2, 'concat') as string;
+    public getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
 
-// Be careful because TS sees no problem here but a string is return
-let nextVal: number = addOrConcat(2, 2, 'concat') as number;
+const Dave = new Coder('Dave', 'Rock', 42);
 
-// got an error because a number can be a string
-10 as string;
-// double casting or Force casting
-10 as unknown as string;
+console.log(Dave.getAge());
+// console.log(Dave.age); // error because age is private
+// console.log(Dave.lang); // error because age is protected
 
-// the DOM
-const img = document.querySelector('img')!; // TS is more specific here and say that it's a HTMLImageElement
-const myImgId = document.querySelector('#myImg') as HTMLImageElement; // TS is less specific and say that it's an Element
-const nextImgId = <HTMLImageElement>document.querySelector('#myImg');
+class WebDev extends Coder {
+    constructor(
+        public computer: string,
+        name: string,
+        music: string,
+        age: number
+    ) {
+        super(name, music, age); // super needs to come before we try to assign anything else
+        this.computer = computer;
+    }
 
-// we will have an error if we remove the exclamation mark,
-// because by default, the element can be a HTML element or null
-// The exclamation mark is for saying that the element can't be null
-img.src;
-// we will have an error if we remove the assertion
-myImgId.src;
+    public getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+// console.log(Sara.age); // there are an error because age is still private
+
+///////////////////////////////////////////////////////////////
+
+// applying a interface to a class
+
+interface Musician {
+    name: string;
+    instrument: string;
+    play(action: string): string;
+}
+
+class Guitariste implements Musician {
+    name: string;
+    instrument: string;
+
+    constructor(name: string, instrument: string) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+
+    play(action: string) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+
+const Page = new Guitariste('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+
+////////////////////////////////////////////////////////////////////
+
+class Peeps {
+    // count is not apply to any instantiation of the class
+    // It applies to the class directly itself
+    static count: number = 0;
+
+    // getCount will be call directly in the class as well
+    static getCount(): number {
+        return Peeps.count;
+    }
+
+    public id: number;
+    constructor(public name: string) {
+        this.name = name;
+        // first id will be 1.
+        // Peeps.count++ => the first id will be 0
+        this.id = ++Peeps.count;
+    }
+}
+
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+
+// The count is incrementing as much as Peeps class is instantiated
+console.log(Peeps.count);
+console.log(`The Id of Steve is ${Steve.id}`);
+console.log(`The Id of Amy is ${Amy.id}`);
+console.log(`The Id of John is ${John.id}`);
+
+////////////////////////////////////////
+// getters and setters
+
+class Bands {
+    private dataState: string[];
+
+    constructor() {
+        this.dataState = [];
+    }
+
+    public get data(): string[] {
+        return this.dataState;
+    }
+
+    public set data(value: string[]) {
+        if (
+            Array.isArray(value) &&
+            value.every((el) => typeof el === 'string')
+        ) {
+            // return this.dataState = value // setters can't return a value
+            this.dataState = value;
+            return;
+        } else throw new Error('Param is not an array of strings');
+    }
+}
+
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+// MyBands.data = 'Van Halen'; // We've got an error because it's not an array
+// MyBands.data = ['Van Halen', 5150]; // We've got the error in the console because there are a number in the array
