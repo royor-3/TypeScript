864fbb7fe8151a75dcba3b3641ceb410dd302178
royor-3 <roy.framery@gmail.com>
2023-06-01 23:55:37 +0200

    remove unecessary file

--- src/hooks/useCart.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import CartContext from "../context/CartProvider";
-import { UseCartContextType } from "../context/CartProvider";
-
-const useCart = ():UseCartContextType => {
-    return useContext(CartContext)
-}
-
-export default useCart
--- src/hooks/useProducts.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import ProductsContext from "../context/ProductsProvider";
-import { UseProductsContextType } from "../context/ProductsProvider";
-
-const useProducts = ():UseProductsContextType => {
-    return useContext(ProductsContext)
-}
-
-export default useProducts
